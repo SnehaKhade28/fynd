fa8d56819a8d826e0e385653b2a8e88fbf48ffb7
SnehaKhade28 <99982807+SnehaKhade28@users.noreply.github.com>
2023-03-21 00:15:07 +0530

    TEST

---
+++ MEVM/vuejs/hello-world/src/components/myvuefile.vue
@@ -0,0 +1,26 @@
+<script setup>
+defineProps({
+  mycomment: {
+    type: String,
+    required: true,
+  },
+});
+</script>
+
+<template>
+  <!-- My FIle  -->
+  <div class="container">
+    <h3>My name is Sneha Tanaji Khade</h3>
+  </div>
+</template>
+
+<style scoped>
+
+.container {
+  text-align: center;
+}
+h3 {
+    margin-top: 40px;
+  color: green;
+}
+</style>
